e272552216b1434b52495d2089e5de3e8346707e
Rob Green <robertma.green@gmail.com>
2022-05-03 14:14:04 +0100

    movie index css

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,13 +3,27 @@ import OwenFace from './images/OwenFace.webp'
 import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
+  
+
   return (
-    <Box boxSize='sm'>
-      <Link to='/movies'>
-        <Image src={OwenFace} alt='Owen Wilson' />
-      </Link>
+    <div className="wrapper">
+    <Box maxW='sm' borderWidth='1px' overflow='hidden'>
+      <Image width='100%' src='https://upload.wikimedia.org/wikipedia/en/3/3e/Wedding_crashers_poster.jpg' alt='' />
+
+      <Box >
+        
+        
+      </Box>
     </Box>
+    </div>
   )
 }
 
 export default LandingPage
+
+
+{/* <Box boxSize='sm'>
+      <Link to='/movies'>
+        <Image src={OwenFace} alt='Owen Wilson' />
+      </Link>
+    </Box> */}
--- src/components/MovieIndex.js
+++ src/components/MovieIndex.js
@@ -2,31 +2,21 @@
 import { Link } from "react-router-dom"
 
 //chakra
-import { Box, Image, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Image, SimpleGrid } from '@chakra-ui/react'
 
 const MovieIndex = () => {
+  
   return (
     <div className="wrapper">
-      <SimpleGrid minChildWidth='250px' spacing='40px' width={900}>
-        <Box bg='tomato' height='450px' >
-          <Box>
-            {/* image */}
-            
-              <Image width='100%'  maxH='450px' objectFit='cover'  src='https://upload.wikimedia.org/wikipedia/en/3/3e/Wedding_crashers_poster.jpg'/>
-              
-          </Box>
-          <Box align='center' mt='10px'>
-            {/* Text */}
-            <Text >
-             Wedding Crashers (2005)
-            </Text>
-          </Box>
-        </Box>
-        <Box bg='tomato' height='450px'></Box>
-        <Box bg='tomato' height='450px'></Box>
-        <Box bg='tomato' height='450px'></Box>
-        <Box bg='tomato' height='450px'></Box>
-        <Box bg='tomato' height='450px'></Box>
+      <SimpleGrid columns={{sm:1, md:2, lg:3}} spacing='40px' width='900px'>
+      <Box maxW='sm' borderWidth='1px' overflow='hidden' rounded='10px' borderColor='gray.300'>
+        <Link to='/'>
+      <Image width='100%' src='https://upload.wikimedia.org/wikipedia/en/3/3e/Wedding_crashers_poster.jpg' alt='' />
+      </Link>
+      <Box p={2} textAlign='center'>
+        Wedding Crashers (2005)
+      </Box>
+    </Box>
       </SimpleGrid>
     </div>
 
