fa25886ac793f61d6bc2ed08a75e3041c410b86d
Rob Green <robertma.green@gmail.com>
2022-05-03 10:34:22 +0100

    chakra ui

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+
+//react router dom v6
+//BrowserRouter contains components which use links
+//Routes allows us to only load one component at a time
+//Route specifies a route and attached to a component
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//import components
+
+
+
+
+
+
+
+const App = () => {
+
+
+
+  return (
+    
+      <h1>hello</h1>
+    
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import './styles/main.scss'
+import App from './App'
+
+createRoot(document.getElementById('root')).render(<ChakraProvider><App /></ChakraProvider>)
